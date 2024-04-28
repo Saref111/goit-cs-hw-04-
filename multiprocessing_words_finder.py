d646f50d4dd52399ff80d15a0729cb540be33bc5
Saref111 <saref012@gmail.com>
2024-04-28 08:19:56 +0300

    fixes

--- multiprocessing_words_finder.py
+++ multiprocessing_words_finder.py
@@ -1,14 +1,23 @@
 from multiprocessing import Process, Queue
 import os
+import logging  
+from math import ceil
 
 def process_files(files_list, words, queue):
     for file in files_list:
-        with open(file, 'r') as f:
-            for line in f:
-                for word in words:
-                    if word in line:
-                        queue.put(f'Event in process {os.getpid()}: {word} found in {file}')
-                        break
+        try:
+            with open(file, 'r') as f:
+                for line in f:
+                    for word in words:
+                        if word in line:
+                            queue.put(f'Event in process {os.getpid()}: {word} found in {file}')
+                            break
+        except FileNotFoundError as e:
+            logging.error(f'File {file} not found')
+        except PermissionError as e:
+            logging.error(f'Permission denied for {file}')
+        except Exception as e:
+            logging.error(f'Unexpected error with file {file}: {str(e)}')
 
 if __name__ == '__main__':
     files = ['./files/file_1.txt', './files/file_2.txt', './files/file_3.txt', './files/file_4.txt', './files/file_5.txt', './files/file_6.txt']
@@ -16,8 +25,14 @@ if __name__ == '__main__':
     queue = Queue()
     processes = []
     
-    for i in range(0, len(files)):
-        process = Process(target=process_files, args=([files[i]], words, queue))
+    num_processes = os.cpu_count()
+    files_per_process = ceil(len(files) / num_processes)
+
+    for i in range(num_processes):
+        start = i * files_per_process
+        end = min((i + 1) * files_per_process, len(files))
+        process_files_list = files[start:end]
+        process = Process(target=process_files, args=(process_files_list, words, queue))
         process.start()
         processes.append(process)
     
--- threading_words_finder.py
+++ threading_words_finder.py
@@ -1,21 +1,40 @@
-from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
+import logging
+from collections import defaultdict
 
-def process_files(files_list, words):
-    for file in files_list:
+def process_files(file, words):
+    results = {}
+    try:
         with open(file, 'r') as f:
             for line in f:
                 for word in words:
                     if word in line:
                         print(f'Event in thread: {word} found in {file}')
+                        if word in results:
+                            results[word] += 1
+                        else:
+                            results[word] = 1
                         break
+    except FileNotFoundError as e:
+        logging.error(f'File {file} not found')
+    except PermissionError as e:
+        logging.error(f'Permission denied for {file}')
+    except Exception as e:
+        logging.error(f'Unexpected error with file {file}: {str(e)}')
+    return results
 
 if __name__ == '__main__':
     files = ['./files/file_1.txt', './files/file_2.txt', './files/file_3.txt', './files/file_4.txt', './files/file_5.txt', './files/file_6.txt']
     words = ['Lorem', 'Suspendisse', 'Cras', 'Maecenas', 'Proin', 'Duis']
-    thread1 = Thread(target=process_files, args=(files[:3], words))
-    thread2 = Thread(target=process_files, args=(files[3:], words))
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
+    results = {}
+
+    with ThreadPoolExecutor() as executor:
+            results = list(executor.map(process_files, files, [words]*len(files)))
+
+    merged_results = defaultdict(int)
+    for result in results:
+        for key, value in result.items():
+            merged_results[key] += value
+
     print('Done')
+    print(merged_results)
